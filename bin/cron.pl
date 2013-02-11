bc8dfa0acfa907ff8d48588a0e7c2dfdf45cfc76
larrythecow <sid@projekt-turm.de>
2013-02-11 18:28:53 +0000

    bugfix

--- bin/cron.pl
+++ bin/cron.pl
@@ -14,30 +14,36 @@ my $logPath="/opt/tmp/";
 my $temperature;
 my $humidity;
 my $fh;
+my $debug=1;
 
 
 my $conf = YAML::XS::LoadFile("$path/sensor.yml");
 my %yml = %$conf;
 
 foreach my $type(keys %yml){
-        print "\t$type\n";
+	 if($debug >= 1){
+	        print "\t$type\n";
+		}
 	my $i=0;
                 foreach my $id ( @{$yml{$type}->{id}} ){
                         if($type eq "dht11"){
                                 ($temperature, $humidity) =  fillArray($func{$type}, 3, 3, $id);
-                                print "\t\t$yml{$type}->{name}->[$i]\t$id\t$temperature $humidity\n";
-				$yml{$type}->{'temperature'}->[$i]= "$temperature";
 				$yml{$type}->{'humidity'}->[$i]= "$humidity";
-				$yml{$type}->{'time'}->[$i] = time;
                         }
                         else{  
                                 $temperature =  fillArray($func{$type}, 3, 3, $id);
-                                print "\t\t$yml{$type}->{name}->[$i]\t$id\t$temperature [$i]\n";
-				$yml{$type}->{'temperature'}->[$i]= "$temperature";
-				$yml{$type}->{'time'}->[$i] = time;
                         }
+		$yml{$type}->{'temperature'}->[$i]= "$temperature";
+		$yml{$type}->{'time'}->[$i] = time;
+		if($debug >= 1){
+			if($type eq "dht11"){
+				print "\t\t$yml{$type}->{name}->[$i]\t$id\t$temperature $humidity\n";
+				}
+			print "\t\t$yml{$type}->{name}->[$i]\t$id\t$temperature [$i]\n";
+			}
 		$i++;
                 }
 }
 YAML::XS::DumpFile("$logPath/test.yml", $conf);
+
 `$path/bin/net/send.pl`;
